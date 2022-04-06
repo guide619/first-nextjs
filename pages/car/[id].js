f06e97a374ba6b6cd97c730d319e7d21dc977f0a
thayakorn.fu <guide619@gmail.com>
2022-04-06 17:34:40 +0700

    SSG

--- pages/car/[id].js
+++ pages/car/[id].js
@@ -3,34 +3,34 @@ import Head from 'next/head'
 
 const url = process.env.SERVER_URL
 
-export async function getServerSideProps({ params }){
-    const req = await fetch(`${url}/${params.id}.json`);
-    const data = await req.json();
-    return {
-        props: { car : data}
-    }
-}
-
-// export async function getStaticProps({ params }){
-//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+// export async function getServerSideProps({ params }){
+//     const req = await fetch(`${url}/${params.id}.json`);
 //     const data = await req.json();
 //     return {
 //         props: { car : data}
 //     }
 // }
 
-// export async function getStaticPaths() {
-//     const req = await fetch("http://localhost:3000/car.json");
-//     const data = await req.json();
+export async function getStaticProps({ params }){
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+    return {
+        props: { car : data}
+    }
+}
 
-//     const paths = data.map(car => {
-//         return {params : { id : car }}
-//     })
-//     return {
-//         paths,
-//         fallback : false
-//     }
-// }
+export async function getStaticPaths() {
+    const req = await fetch("http://localhost:3000/car.json");
+    const data = await req.json();
+
+    const paths = data.map(car => {
+        return {params : { id : car }}
+    })
+    return {
+        paths,
+        fallback : false
+    }
+}
 
 export default function Car({ car } ){
     const route = useRouter()
