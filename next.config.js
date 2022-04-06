ca0f63e237353216af47103137d466724bca43b6
thayakorn.fu <guide619@gmail.com>
2022-04-06 17:16:28 +0700

    fix url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   env: {
-    SERVER: process.env.SERVER,
+    VERCEL_URL: process.env.VERCEL_URL,
   },
 }
 
